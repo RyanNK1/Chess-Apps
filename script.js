7d7da0a5c7d10db732ea9a8f51dea24c46321c29
ryan <ryan.kimani0@gmail.com>
2025-07-09 11:53:43 +0800

    1.Added the dragover and drop event listeners and called the allowDrop and drop functions. 2.Created Square ids(row and column) for each square.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let legalSquares = [];
+const boardSquares = document.getElementsByClassName("square");
+const pieces = document.getElementsByClassName("piece");
+const piecesImages = document.getElementsByClassName("img")
+
+function setupBoardSquares(){
+    for (let i=0; i<boardSquares.length;i++){
+        boardSquares[i].addEventListener("dragover" , allowDrop);
+        boardSquares[i].addEventListener("drop" , drop);
+        let row= 8-Math.floor(i/8);
+        let column=String.fromCharCode(97+(i/8));
+        let square=boardSquares[i];
+        square.id = column+row
+    }
+}
+
 
